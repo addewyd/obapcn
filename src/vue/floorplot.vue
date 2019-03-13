a010c6bc37b9a076d2fa1eb6bb0d59c3416ef244
addewyd <addewyd@mail.ru>
2019-03-13 08:05:49 +0400

    floor plot new comp

---
+++ src/vue/floorplot.vue
@@ -0,0 +1,24 @@
+﻿<template>
+<div>
+    <h3>
+{{floorplot}}
+    </h3>
+    {{floorid}}
+    <button class="btn btn-primary" @click="close">Close</button>
+</div>
+</template>
+<script>
+
+module.exports = {
+    props: {
+          floorid: String,
+            floorplot: String
+    },
+
+    data: function() {
+        return {
+        }
+    }
+}
+
+</script>
